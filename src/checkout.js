ed867078a687eeb7b6f5b3f1fbb1f277c03db779
Sam Goldsmith <samggold@gmail.com>
2016-07-28 14:29:50 -0600

    functional checkout page

--- src/checkout.js
+++ src/checkout.js
@@ -1,44 +1,4 @@
 $(document).ready(function() {
-  $('#personalInfo').on('submit', function(event) {
-    event.preventDefault();
-    var error = false;
-    // Get the values
-    var ccNum = $('#creditNum').val().replace(/-/g, ''),
-    cvcNum = $('#cvcNum').val(),
-    expMonth = $('#expiryMonth').val(),
-    expYear = $('#expiryYear').val();
-    // Validate the number
-    if (!Stripe.card.validateCardNumber(ccNum)) {
-      error = true;
-      console.log('The credit card number appears to be invalid.');
-    }
-    // Validate the CVC
-    if (!Stripe.card.validateCVC(cvcNum)) {
-      error = true;
-      console.log('The CVC number appears to be invalid.');
-    }
-    // Validate the expiration
-    if (!Stripe.card.validateExpiry(expMonth, expYear)) {
-      error = true;
-    }
-    // Get the Stripe token
-    if (!error) {
-      Stripe.card.createToken({
-        number: ccNum,
-        cvc: cvcNum,
-        exp_month: expMonth,
-        exp_year: expYear
-      }, stripeResponseHandler);
-    }
-    function stripeResponseHandler(status, response) {
-      if (response.error) {
-        console.log(response.error.message);
-      } else { // No errors, submit the form.
-        console.log('form submitted successfully!');
-      }
-    }
-    });
-  });
   validateTextField('#lastNameShipping');
   validateTextField('#lastNameBilling');
   validateTextField('#address1Shipping');
@@ -56,7 +16,58 @@ $(document).ready(function() {
     var state = $('#stateShipping option:selected').val();
     $('#stateBilling option[value=' + state + ']').attr('selected','selected');
   });
-
+  $('#personalInfo').on('submit', function(event) {
+    event.preventDefault();
+    var error = false;
+    // Get the values
+    var ccNum = $('#creditNum').val().replace(/-/g, ''),
+    cvcNum = $('#cvcNum').val(),
+    expMonth = $('#expiryMonth').val(),
+    expYear = $('#expiryYear').val();
+    // Validate the number
+    if (!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      console.log(ccNum);
+      console.log('The credit card number appears to be invalid.');
+      var errorMessage = 'Uh oh. Your credit card number appears to be invalid. Please enter a valid number and resubmit.'
+      console.log('The CVC number appears to be invalid.');
+      alertErrorMessage(errorMessage);
+    }
+    // Validate the CVC
+    if (!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      var errorMessage = 'The CVC number appears to be invalid. Please enter a valid number and resubmit.'
+      console.log('The CVC number appears to be invalid.');
+      alertErrorMessage(errorMessage);
+    }
+    // Validate the expiration
+    if (!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      var errorMessage = 'The expiration date appears to be invalid. Please enter a valid month and year and resubmit.';
+      console.log('The expiration date appears to be invalid.');
+      alertErrorMessage(errorMessage);
+    }
+    // Get the Stripe token
+    if (!error) {
+      Stripe.card.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    function stripeResponseHandler(status, response) {
+      if (response.error) {
+        console.log(response.error.message);
+      } else { // No errors, submit the form.
+        console.log('form submitted successfully!');
+        var successMessage = 'Your purchase has been successfully processed!';
+        //$("#personalInfo")[0].reset();
+        alertSuccessMessage(successMessage);
+      }
+    }
+  });
+});
 //Validation Functions
 //Function to validate text field
 function validateTextField(id) {
@@ -79,4 +90,11 @@ function validateZipField(id) {
       $(this).css({'border-color': '', 'box-shadow': ''});
     }
   });
+}
+//Apply error message
+function alertErrorMessage(msg) {
+  $("#submitMessage").append('<div class="alert alert-dismissible alert-danger"><strong>' + msg + '</stron></div>').delay(3000).fadeOut(500).removeClass('');
+}
+function alertSuccessMessage(msg) {
+  $("#submitMessage").append('<div class="alert alert-dismissible alert-success"><strong>' + msg + '</stron></div>').delay(3000).fadeOut(500).removeClass('');
 }
